072bda0a86932235192d916e50db913fad0475fa
LucasJen <fake@fake.com>
2026-02-26 11:24:21 -0600

    express app init

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router() // figures out what code to run in response to a request
+// typically based on the URL, and the method, GET, POST ....
+
+
+// responds to get request to home page /
+router.get('/', function(requ, res, next){ // request, response, next
+    // name of Handlebars file - name of a template, optional object with data for the template
+    res.render('index', {
+        title: 'Feedback Application',
+        author: 'Lucas',
+        timePageLoadedAt: new Date()
+    })
+}) //get request to the home page
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+
+const indexRouter = require('./routes/index.js')
+const app = express() // creates the web app server
+
+// tell app where the views (HTML files or templates) are
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs') // user handelbares to generate views
+
+app.use('/', indexRouter) // all request to the app will be passed to indexRouter
+
+//start server running
+const server = app.listen(process.env.PORT || 3000, function() {
+    console.log('Server running on port ', server.address().port)
+})
